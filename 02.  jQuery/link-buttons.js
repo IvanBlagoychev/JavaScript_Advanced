56c331cfe948410927551778e38e0abe39f534aa
Ivan Blagoychev <ivanblagoychev@gmail.com>
2017-05-10 21:57:50 +0300

    Tasks

---
+++ 02.  jQuery/link-buttons.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Hristina on 24.10.2016 г..
+ */
+function attachEvents() {
+    $('a.button').on('click', buttonClicked);
+    function buttonClicked() {
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
+     }
+}
